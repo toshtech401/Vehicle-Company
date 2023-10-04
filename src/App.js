82eb0fb0f0cc1d701b6b0dae2b28e59338d71b70
unknown <mustaphas902@gmail.com>
2023-10-04 11:59:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      hello
     </div>
   );
 }
